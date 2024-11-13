594032279c175613e156efa0200bde3289ba1d54
Manohar D S <manohar@kali>
2024-11-14 00:18:41 +0530

    finished queue using cll updated

--- SEM-3/DSA/addSLL2.c
+++ SEM-3/DSA/addSLL2.c
@@ -4,9 +4,9 @@
 ok so during our class today I was able to  figured out few methods to add 2 ints using LLs.
 This is one of it, basically the simplest noobie method.
 1. Take 2 numbers as strings(check if they are valid numbers)
-2. Based on the len of 2 strings(basically no of digits) creating a CSLL for both.
+2. Based on the len of 2 strings(basically no of digits) creating current CSLL for both.
 3. Then traverse thru both the lists until they reach their respective last node(tail node) add their respective digits and insert the sum as node to the SUM LL.
-4. if there's a carry left out then add it another node to the SUM LL.
+4. if there's current carry left out then add it another node to the SUM LL.
 5. then display the sum thats it.
 
 NOTE:Tail Node wont be considered for addition; its just to mark the lastnode, its possible to implement without using this tail node
@@ -62,63 +62,45 @@ int main(int argc, char const *argv[]) {
     scanf("%s", num2);
 
     size_t len1 = strlen(num1), len2 = strlen(num2);
-    Node A = createList(), B = createList();
-
+    Node SUM = createList();
+    short carry = 0;
     // Populate linked lists A and B with digits of num1 and num2 in reverse order
-    for (int i = 0, j = 0; i < len1 || j < len2; i++, j++) {
-        if (i < len1) {
-            A = insertStart(A, num1[i] - '0');
+    for (short i = len1, j = len2, sum = 0; i >= 0 || j >= 0 || carry != 0; i--, j--) {
+        if (i > -1 && j > -1) {
+            sum = ((num2[j] - '0') + (num1[i] - '0') + carry ) % 10;
+            carry = ((num2[j] - '0') + (num1[i] - '0') + carry) / 10;
+        } else if (j > -1) {
+            sum = ((num1[i] - '0') + carry ) % 10;
+            carry = ((num1[i] - '0') + carry) / 10;
+        } else if(i > -1) {
+            sum = ((num2[j] - '0') + carry ) % 10;
+            carry = ((num2[j] - '0')    + carry) / 10;
         }
-        if (j < len2) {
-            B = insertStart(B, num2[j] - '0');
-        }
+        SUM = insertStart(SUM, sum);
     }
 
-    // Sum linked lists A and B to produce the SUM list
-    Node SUM = createList(), a = A->next, b = B->next;
-    short carry = 0, sum = 0;
-
-    // Traverse both lists and add corresponding digits
-     while(a != A || b != B){
-        sum = carry; // Start with carry value
-
-        if(a != A) { // If there are digits left in list A
-            sum += a->digit;
-            a = a->next;
-        }
-
-        if(b != B) { // If there are digits left in list B
-            sum += b->digit;
-            b = b->next;
-        }
-
-        carry = sum / 10; // Calculate the new carry
-        sum = sum % 10; // Get the digit to store in SUM
-
-        SUM = insertStart(SUM, sum); // Insert the sum digit
+    if (carry != 0) {  // Add any remaining carry as current new node
+        SUM = insertStart(SUM, carry);
     }
 
-    // If there's still a carry left after the loop
-    if (carry != 0){
-        SUM = insertStart(SUM, carry); // Add the carry as a new node
-    }
-
-    // Display the sum
-    a = SUM->next;
-    printf("The SUM is ");
+    // Display the final sum
+    Node current = SUM->next;
+    printf("\nThe SUM is ");
     do {
-        printf("%d", a->digit);
-        a = a->next;
-    } while(a != SUM);
+        printf("%d", current->digit);
+        current = current->next;
+    } while (current != SUM);
     printf("\n\n");
 
-    frees(A);
-    frees(B);
+    // Free all allocated memory
     frees(SUM);
+
     return 0;
 }
+
+
 /*And the other methods are,
-1. using a CDLL
+1. using current CDLL
 2. Initializing two LL for storing numbers and the using one of those two for storing the sum also
 3. scanning 2 i/p strings and while traversing each of them creating one CSLL that stores the sums directly(no lists for storing numbers)
 
